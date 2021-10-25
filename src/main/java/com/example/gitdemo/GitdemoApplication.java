a29ec145d615cb87bf492adaff43950508722dce
CHawkins92 <conor.hawkins@gmx.com>
2021-10-25 10:23:17 +0000

    Implement the greet method
    add empty method with hello endpoint
    
    Add @RestController

--- src/main/java/com/example/gitdemo/GitdemoApplication.java
+++ src/main/java/com/example/gitdemo/GitdemoApplication.java
@@ -2,12 +2,20 @@ package com.example.gitdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GitdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitdemoApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    String greet(){
+        return "Hello, World!";
+    }
 }
+
